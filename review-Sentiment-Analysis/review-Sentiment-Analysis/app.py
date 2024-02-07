5d56e5d413d75f9e09315c0232c61ed1ec4a991b
Gaurav singh <130684251+Gaurav7506@users.noreply.github.com>
2024-02-07 22:48:36 +0530

    Add files via upload

---
+++ review-Sentiment-Analysis/review-Sentiment-Analysis/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import joblib
+
+app = Flask(__name__)
+
+# Load models
+with open('Naive_Bayes_model_imdb.pkl', 'rb') as f:
+    mnb = pickle.load(f)
+
+with open('countVect_imdb.pkl', 'rb') as f:
+    countVect = pickle.load(f)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        Reviews = request.form['Reviews']
+        data = [Reviews]
+        vect = countVect.transform(data).toarray()
+        my_prediction = mnb.predict(vect)
+        return render_template('result.html', prediction=my_prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
